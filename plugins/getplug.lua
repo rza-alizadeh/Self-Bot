256ce04b80c6a44bf1383c3ffe22f5a313998c2c
rza-alizadeh <rza.alizadeh78@gmail.com>
2016-10-06 18:33:03 +0330

    Update getplug.lua

--- plugins/getplug.lua
+++ plugins/getplug.lua
@@ -1,7 +1,7 @@
 do
 
 local function run(msg, matches)
-  if matches[1] == "getplug" then
+  if matches[1] == "send" then
     local file = matches[2]
     if is_sudo(msg) then
       local receiver = get_receiver(msg)
@@ -12,7 +12,7 @@ end
 
 return {
   patterns = {
-  "^#(getplug) (.*)$"
+  "^[!#/](send) (.*)$"
   },
   run = run
 }
